54054b0ec8ab390d7347db07e51dd76a206cab3f
Mike Lorengo <mikelor@gmail.com>
2022-04-03 12:47:50 -0700

    First UpdateInital Version of Tripster App

---
+++ TripsterApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TripsterApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ TripsterApp/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Maui.Essentials;
+
+namespace TripsterApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+            CounterLabel.Text = $"Current count: {count}";
+
+            SemanticScreenReader.Announce(CounterLabel.Text);
+
+
+            var myLocation = await Geolocation.GetLocationAsync();
+            var otherLocation = new Location(47.32635424270523, -122.00971100077268);
+
+            var distance = myLocation.CalculateDistance(otherLocation, DistanceUnits.Miles);
+
+            CounterLabel.Text = $"Distance to Lumber House: {distance:0.##} Miles";
+        }
+    }
+}
